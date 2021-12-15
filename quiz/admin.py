f02ee6a68ed2712c6146e375a7be381bbc5e8abe
cristoblanco <cristo_blanco@hotmail.com>
2021-12-14 21:18:53 -0500

    agregando administrador

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import register
 
+from quiz.models import Question, Response
+
+admin.site.register(Question)
+admin.site.register(Response) 
 # Register your models here.
